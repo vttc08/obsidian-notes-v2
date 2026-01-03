d511fa5d1fee552bceab1ac5bb1c293f77ef46f3
Kevin Hu <hukevin69@gmail.com>
2026-01-02 19:39:06 -0800

    add git integration in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,10 @@
 # Deploy the Obsidian Publish site
 cd $(dirname ${BASH_SOURCE[0]})
 
+rsync -ahP --delete ~/Documents/notes/ ./content/
+
 docker compose run --rm quartz-builder
+
+git add ./content
+git commit -m "Page updated in Obsidian $(date +"%Y-%m-%dT%H:%M:%S%Z")"
+git push origin
